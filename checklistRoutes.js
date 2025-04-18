f0f3896ad84e53f74c337a453a51d16bd1681bd3
sunan90 <sasafatimah19@gmail.com>
2025-04-18 22:14:40 +0700

    Update checklistRoutes.js
    Signed-off-by: sunan90 <sasafatimah19@gmail.com>

--- checklistRoutes.js
+++ checklistRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const checklistController = require('../controllers/checklistController');
 
 router.get('/', checklistController.getAllChecklist);
+router.get('/rekap', checklistController.getRekapData);
 router.post('/', checklistController.addChecklist);
 router.put('/:id', checklistController.updateChecklist);
 router.delete('/:id', checklistController.deleteChecklist);
