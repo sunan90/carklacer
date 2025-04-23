ba29959183815a407d2a8a28c3472228e3f56857
sunan90 <sasafatimah19@gmail.com>
2025-04-24 00:21:21 +0700

    Update server.js
    Signed-off-by: sunan90 <sasafatimah19@gmail.com>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { verifyToken } = require('./authMiddleware');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
