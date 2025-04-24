d02111bccd6b1d8e6c8708e8664e902afa080d66
sunan90 <sasafatimah19@gmail.com>
2025-04-24 13:57:36 +0700

    Update authRoutes.js
    Signed-off-by: sunan90 <sasafatimah19@gmail.com>

--- authRoutes.js
+++ authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('./authController');
+const checklistController = require('./checklistController');
 
 router.post('/login', authController.login);
+router.get('/rekap', checklistController.getRekapData); // Rute baru untuk admin
 
 module.exports = router;
